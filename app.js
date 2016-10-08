9a3524d4a1bca5d260b6884c0e79bb743bdc83aa
Taylor Dolan <taylordolan@me.com>
2016-10-08 11:30:35 -0700

    refactor + remove scoring stuff for now

--- app.js
+++ app.js
@@ -1,202 +1,137 @@
 var map;
-var score;
+var board = [];
 var tiles;
 
-var kobold = document.createElement("div");
-kobold.innerText = "k";
-kobold.classList.add("char");
-
-var hero = document.createElement("div");
-hero.classList.add("char", "char--hero");
-hero.innerText = "@";
-
-var star = document.createElement("div");
-star.classList.add("char", "char--star");
-star.innerText = "🌟";
-
-function spawnKobold() {
-  deployToRandomEmptyTile(kobold);
+for (var i = 0; i < 16; i++) {
+  board[i] = [];
 }
 
-function advanceKobold() {
-  var hRow = getRow(hero);
-  var hCol = getCol(hero);
-  var hTile = getTile(hero);
-  var kRow = getRow(kobold);
-  var kCol = getCol(kobold);
-  var kTile = getTile(kobold);
+function Item() {
 
-  if (kRow === hRow) {
-    if (kTile > hTile) {
-      moveLeft(kobold);
+  this.deployToRandomEmptyTile = function() {
+    emptyTiles = [];
+    for (var i=0; i<board.length; i++) {
+      if (board[i].length === 0) {
+        emptyTiles.push(board[i]);
+      }
+    }
+    emptyTiles[Math.floor(Math.random()*emptyTiles.length)].push(this);
+  }
+
+  this.getTile = function() {
+    for (var i = 0; i<board.length; i++) {
+      for (var j = 0; j<board[i].length; j++) {
+        if (board[i][j] == this) {
+          return i;
+        }
+      }
+    }
+  }
+
+  this.getCol = function() {
+    if (this.getTile() === 0 || this.getTile() % 4 === 0) {
+      return 0;
+    } else if (this.getTile() === 1 || this.getTile() === 5 || this.getTile() === 9 || this.getTile() === 13) {
+      return 1;
+    } else if (this.getTile() === 2 || this.getTile() === 6 || this.getTile() === 10 || this.getTile() === 14) {
+      return 2;
+    } else if (this.getTile() === 3 || this.getTile() === 7 || this.getTile() === 11 || this.getTile() === 15) {
+      return 3;
+    }
+  }
+
+  this.getRow = function() {
+    if (this.getTile() >= 0 && this.getTile() <= 3) {
+      return 0;
+    } else if (this.getTile() >= 4 && this.getTile() <= 7) {
+      return 1;
+    } else if (this.getTile() >= 8 && this.getTile() <= 11) {
+      return 2;
+    } else if (this.getTile() >= 12 && this.getTile() <= 15) {
+      return 3;
+    }
+  }
+
+  this.setTile = function(tileNumber) {
+    board[this.getTile()].pop(this);
+    board[tileNumber].push(this);
+    renderBoard();
+  }
+
+  this.moveRight = function() {
+    if (this.getCol() < 3) {
+      this.setTile(this.getTile() + 1);
+    }
+    renderBoard();
+  }
+
+  this.moveLeft = function() {
+    if (this.getCol() > 0) {
+      this.setTile(this.getTile() - 1);
+    }
+    renderBoard();
+  }
+
+  this.moveUp = function() {
+    if (this.getRow() > 0) {
+      this.setTile(this.getTile() - 4);
+    }
+    renderBoard();
+  }
+
+  this.moveDown = function() {
+    if (this.getRow() < 3) {
+      this.setTile(this.getTile() + 4);
+    }
+    renderBoard();
+  }
+
+  // to be used like this, for example: hero.moveWithOption("moveRight", "moveDown")
+  this.moveWithOption = function(opt1, opt2) {
+    if (Math.floor(Math.random() * 2) == 0) {
+      this[opt1]();
     } else {
-      moveRight(kobold);
-    }
-  } else if (kCol === hCol) {
-    if (kTile > hTile) {
-      moveUp(kobold);
-    } else {
-      moveDown(kobold);
-    }
-  } else if (kCol > hCol) {
-    if (kRow > hRow) {
-      moveLeftOrUp(kobold);
-    } else {
-      moveLeftOrDown(kobold);
-    }
-  } else if (kCol < hCol) {
-    if (kRow > hRow) {
-      moveRightOrUp(kobold);
-    } else {
-      moveRightOrDown(kobold);
+      this[opt2]();
     }
   }
 }
 
-function getTile(char) {
-  for (i=0; i<tiles.length; i++) {
-    if (char.parentNode == tiles[i]) {
-      return i;
+var hero = new Item();
+
+function renderBoard() {
+  var tiles = document.getElementsByClassName("tile");
+  for (var i=0; i<tiles.length; i++) {
+    while (tiles[i].firstChild) {
+      tiles[i].removeChild(tiles[i].firstChild);
     }
   }
-}
-
-function getCol(char) {
-  if (getTile(char) === 0 || getTile(char) % 4 === 0) {
-    return 0;
-  } else if (getTile(char) === 1 || getTile(char) === 5 || getTile(char) === 9 || getTile(char) === 13) {
-    return 1;
-  } else if (getTile(char) === 2 || getTile(char) === 6 || getTile(char) === 10 || getTile(char) === 14) {
-    return 2;
-  } else if (getTile(char) === 3 || getTile(char) === 7 || getTile(char) === 11 || getTile(char) === 15) {
-    return 3;
-  }
-}
-
-function getRow(char) {
-  if (getTile(char) >= 0 && getTile(char) <= 3) {
-    return 0;
-  } else if (getTile(char) >= 4 && getTile(char) <= 7) {
-    return 1;
-  } else if (getTile(char) >= 8 && getTile(char) <= 11) {
-    return 2;
-  } else if (getTile(char) >= 12 && getTile(char) <= 15) {
-    return 3;
-  }
-}
-
-function deployToRandomEmptyTile(char) {
-  var emptyTiles = [];
-  for(i=0; i<tiles.length; i++) {
-    if (tiles[i].childNodes.length === 0) {
-      emptyTiles.push(tiles[i]);
+  for (var i=0; i<board.length; i++) {
+    if (board[i][0] == hero) {
+      tiles[i].innerText = "@";
     }
-  }
-  emptyTiles[Math.floor(Math.random()*emptyTiles.length)].appendChild(char);
-}
-
-function moveToTile(char, tile) {
-  char.parentNode.removeChild(char);
-  tiles[tile].appendChild(char);
-  checkForScore();
-  if (char === hero) {
-    advanceKobold();
-  }
-}
-
-function moveUp(char) {
-  if (getRow(char) > 0) {
-    moveToTile(char, (getTile(char) - 4));
-  }
-}
-
-function moveDown(char) {
-  if (getRow(char) < 3) {
-    moveToTile(char, (getTile(char) + 4));
-  }
-}
-
-function moveLeft(char) {
-  if (getCol(char) > 0) {
-    moveToTile(char, (getTile(char) - 1));
-  }
-}
-
-function moveRight(char) {
-  if (getCol(char) < 3) {
-    moveToTile(char, (getTile(char) + 1));
-  }
-}
-
-function moveRightOrDown(char) {
-  if (Math.floor(Math.random() * 2) == 0) {
-    moveRight(char);
-  } else {
-    moveDown(char);
-  }
-}
-
-function moveRightOrUp(char) {
-  if (Math.floor(Math.random() * 2) == 0) {
-    moveRight(char);
-  } else {
-    moveUp(char);
-  }
-}
-
-function moveLeftOrDown(char) {
-  if (Math.floor(Math.random() * 2) == 0) {
-    moveLeft(char);
-  } else {
-    moveDown(char);
-  }
-}
-
-function moveLeftOrUp(char) {
-  if (Math.floor(Math.random() * 2) == 0) {
-    moveLeft(char);
-  } else {
-    moveUp(char);
-  }
-}
-
-function checkForScore() {
-  if (hero.parentNode === star.parentNode) {
-    scoreValue++;
-    score.innerText = scoreValue;
-    star.parentNode.removeChild(star);
-    deployToRandomEmptyTile(star);
   }
 }
 
 window.addEventListener("load", function(){
-  map = document.getElementsByClassName("map")[0];
-  score = document.getElementsByClassName("score")[0];
-  tiles = document.getElementsByClassName("tile");
 
-  scoreValue = 0;
-  score.innerText = scoreValue;
-
-  deployToRandomEmptyTile(hero);
-  deployToRandomEmptyTile(star);
-  spawnKobold();
+  hero.deployToRandomEmptyTile();
+  renderBoard();
 
   document.onkeydown = checkKey;
-
   function checkKey(e) {
     e = e || window.event;
 
     if (e.keyCode == '38') {
-      moveUp(hero);
+      hero.moveUp();
     }
     else if (e.keyCode == '40') {
-      moveDown(hero);
+      hero.moveDown();
     }
     else if (e.keyCode == '37') {
-      moveLeft(hero);
+      hero.moveLeft();
     }
     else if (e.keyCode == '39') {
-      moveRight(hero);
+      hero.moveRight();
     }
   }
 });
